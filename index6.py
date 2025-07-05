740d05f6907043f5b42bc21a9cb92ab7d4975317
Chandanpatel007 <chandanpatel62003@gmail.com>
2025-07-05 19:10:28 +0530

    while loop

---
+++ index6.py
@@ -0,0 +1,19 @@
+i=0
+while i<=3:
+    print('chandu')
+    i=i+1
+
+i=3
+while i>=0:
+    print('chandu')
+    i=i-1
+
+i=2
+while i<=5:
+    print('chandu')
+    i=i=1
+
+i=3
+while i<=6:
+    print('chandu')
+    i+=2
