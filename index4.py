0779359d0ca3aaa802998b64cd3668971a4a0b24
Chandanpatel007 <chandanpatel62003@gmail.com>
2025-07-05 12:42:10 +0530

    for loop

---
+++ index4.py
@@ -0,0 +1,13 @@
+for c in "chandan",23,6.5,True:
+    print(c)
+
+
+for k in 'value',5,88,True:
+    print(k)
+
+
+for i in 'python',1,2,3,4,5,6,7,8,9:
+    print(i)
+
+for j in 'varu','ch','chh','uyv':
+    print(j)
