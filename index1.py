cf7118777985a900479f776c05e34999a51bd6f9
Chandanpatel007 <chandanpatel62003@gmail.com>
2025-07-05 12:14:13 +0530

    conditional statements

---
+++ index1.py
@@ -0,0 +1,4 @@
+a=input(24)
+b=input(24)
+c=input()
+print(a+b+c)
---
+++ index3.py
@@ -0,0 +1,10 @@
+print('enter a value1')
+a=int(input())
+print('enter b value2')
+b=int(input())
+if a > b:
+    print('a is greater')
+elif a < b:
+    print('b is greater')
+else:
+    print('a==b')
